ea583ac424fb0e119d571452737d9874f3bb9ce3
zhangzp <superkusch@foxmail.com>
2025-09-14 17:50:18 +0800

    .gitignore

--- roop/run.py
+++ roop/run.py
@@ -4,3 +4,19 @@ from roop import core
 
 if __name__ == '__main__':
     core.run()
+
+"""
+
+python run.py --execution-provider cpu \
+-s /Users/kusch/PycharmProjects/roop_colab/1.jpg \
+-t '/Users/kusch/PycharmProjects/roop_colab/4.jpg' \
+-o '/Users/kusch/PycharmProjects/roop_colab/hahahahaha.jpg' \
+--frame-processor face_swapper face_enhancer \
+--output-video-encoder libx264 \
+--output-video-quality 35 \
+--keep-fps \
+--many-faces \
+--temp-frame-format jpg \
+--temp-frame-quality 0
+
+"""
